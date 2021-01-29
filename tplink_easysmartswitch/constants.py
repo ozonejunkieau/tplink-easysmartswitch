622d15d712628757069b69e14ef2e8f3f09b690e
Tristan Steele <tristan.steele@redhat.com>
2021-01-29 23:15:32 +1100

    Add existing source.

---
+++ tplink_easysmartswitch/constants.py
@@ -0,0 +1,38 @@
+LINK_STATUS_LOOKUP= {
+    0: "Link Down",
+    1: "Auto",
+    2: "10M Half",
+    3: "10M Full",
+    4: "100M Half",
+    5: "100M Full",
+    6: "1000M Full",
+    7: ""
+}
+
+POE_POWER_STATUS_LOOKUP= {
+    0: "OFF", 
+    1: "TURNING-ON", 
+    2: "ON", 
+    3: "OVERLOAD", 
+    4: "SHORT",
+    5: "NOSTANDARD-PD", 
+    6: "VOLTAGE-HIGH",
+    7: "VOLTAGE-LOW",
+    8: "HARDWARE-FAULT",
+    9: "OVERTEMPERATURE",
+}
+
+POE_PRIORITY_LOOKUP= {
+    2: "Low",
+    1: "Medium",
+    0: "High"
+}
+
+POE_PD_CLASS_LOOKUP= {
+    330: "Class 0",
+    40:  "Class 1",
+    70:  "Class 2",
+    154: "Class 3",
+    300: "Class 4",
+    331: "Inactive",
+}
